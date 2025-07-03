9681beb678d39fe39a6bbd4de705f83b24c71c61
Sadiq <sadiqmemmedov93@mail.ru>
2025-07-03 20:49:38 +0400

    .

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,6 +6,14 @@ plugins {
 }
 
 android {
+    signingConfigs {
+        create("release") {
+            storeFile = file("E:\\Projects\\key storages\\elifba.jks")
+            storePassword = "sadokolik1!Q"
+            keyAlias = "mrsadiq"
+            keyPassword = "sadokolik1!Q"
+        }
+    }
     namespace = "com.example.elifba"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = "27.0.12077973"
@@ -21,7 +29,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.elifba"
+        applicationId = "com.mrsadiq.elifba"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -34,7 +42,7 @@ android {
         release {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
