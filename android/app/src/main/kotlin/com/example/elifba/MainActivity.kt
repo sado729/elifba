2f9e2419295aeca329f102ce89f012aac0dd03ce
Sadiq <sadiqmemmedov93@mail.ru>
2026-03-25 17:00:26 +0400

    feat: enable edge-to-edge display for Android and update app version.

--- android/app/src/main/kotlin/com/example/elifba/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/elifba/MainActivity.kt
@@ -1,5 +1,13 @@
 package com.mrsadiq.elifba
 
+import android.os.Bundle
+import androidx.core.view.WindowCompat
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+    }
+}
+
